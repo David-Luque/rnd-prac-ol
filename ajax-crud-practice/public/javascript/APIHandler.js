5455351b019ed89f42f7edbce22b3b3a3a6a732f
David L <dvdlq99@gmail.com>
2021-03-19 18:02:35 +0100

    setting up axios requests

--- ajax-crud-practice/public/javascript/APIHandler.js
+++ ajax-crud-practice/public/javascript/APIHandler.js
@@ -4,22 +4,87 @@ class APIHandler {
   }
 
   getFullList () {
-
+    axios.get(`${this.BASE_URL}/characters`)
+    .then(response => {
+      const charContainer = document.getElementsByClassName('characters-container')[0];
+      charContainer.innerHTML = '';
+      response.data.forEach(entry => {
+        charContainer.innerHTML += `
+        <div class="character-info">
+          <div class="id">ID: ${entry.id}</div>
+          <div class="name">Name: ${entry.name}</div>
+          <div class="occupation">Occupation: ${entry.occupation}</div>
+          <div class="cartoon">Is a Cartoon?: ${entry.cartoon}</div>
+          <div class="weapon">Weapon: ${entry.weapon}</div>
+        </div>
+        `
+      })
+    })
+    .catch(err => console.log(`ERROR: ${err}`))
   }
 
-  getOneRegister () {
-
+  getOneRegister(id) {
+    axios.get(`${this.BASE_URL}/characters/${id}`)
+    .then(response =>{
+      const charContainer = document.getElementsByClassName('characters-container')[0];
+      charContainer.innerHTML = `
+      <div class="character-info">
+        <div class="id">ID: ${response.data.id}</div>
+        <div class="name">Name: ${response.data.name}</div>
+        <div class="occupation">Occupation: ${response.data.occupation}</div>
+        <div class="cartoon">Is a Cartoon?: ${response.data.cartoon}</div>
+        <div class="weapon">Weapon: ${response.data.weapon}</div>
+      </div>
+      `
+    })
+    .catch(err => {
+      console.log(err)
+    })
   }
 
-  createOneRegister () {
-
+  createOneRegister (register) {
+    axios.post(`${this.BASE_URL}/characters`, register)
+    .then(data =>{
+      console.log(`created successfylly ${data}`);
+      document.getElementById("send-data").style.backgroundColor = 'green';
+    })
+    .catch(err => {
+      console.log(err)
+      document.getElementById("send-data").style.backgroundColor = 'red';
+    })
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(register) {
+    if(!register){
+      axios.get(`${this.BASE_URL}/characters/${register}`)
+      .then(res => console.log(res))
+    } else {
+      console.log('truly')
+    }
+    // axios.put(this.BASE_URL, register)
+    // .then(data => console.log(`updated successfylly ${data}`))
+    // .catch(err => console.log(err))
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    axios.get(`${this.BASE_URL}/characters/${id}`)
+    .then(res => {
+      if(res){
+        axios.delete(`${this.BASE_URL}/characters/${id}`)
+        .then(() => {
+          console.log('successfully deleted');
+          document.getElementById("delete-one").style.backgroundColor = 'green';
+        })
+        .catch(err => {
+          console.log(`error deleting: ${elem.data.name}`);
+          console.log(err);
+          document.getElementById("delete-one").style.backgroundColor = 'red';
+        })
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      document.getElementById("delete-one").style.backgroundColor = 'red';
+    })
   }
 }
--- ajax-crud-practice/public/javascript/index.js
+++ ajax-crud-practice/public/javascript/index.js
@@ -1,23 +1,44 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+  document.getElementById('fetch-all').addEventListener('click', function (event) {   
+    charactersAPI.getFullList();
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+    const searchId = document.querySelectorAll('.operations input')[0].value;
+    charactersAPI.getOneRegister(searchId);
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
+    const deleteId = document.querySelectorAll('.operations input')[1].value;
+    charactersAPI.deleteOneRegister(deleteId);
   });
 
+  document.getElementById('send-update').addEventListener('click', ()=>{
+    const id = document.querySelector('#edit-character-form input').value;
+    charactersAPI.getOneRegister(searchId);
+  })
+
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    console.log('submit')
+    const updateInputs = document.querySelectorAll('#edit-character-form input');
+    const idInput = updateInputs[0].value;
+    charactersAPI.updateOneRegister(idInput);
 
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
+    event.preventDefault();
+    const createInputs = document.querySelectorAll('#new-character-form input');
+    const newCharacter = {
+      name: createInputs[0].value,
+      occupation: createInputs[1].value,
+      weapon: createInputs[2].value,
+      cartoon: createInputs[3].value
+    };
+    console.log(newCharacter)
+    charactersAPI.createOneRegister(newCharacter);
   });
 });
