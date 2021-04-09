c78e30fb81dc47d3cb50e928c6674ad6a4170305
David L <dvdlq99@gmail.com>
2021-04-09 21:17:11 +0200

    created Foodbox component

--- react-nutrition-app/src/App.js
+++ react-nutrition-app/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import 'bulma/css/bulma.css'
 import './App.css';
+import foods from './foods.json';
+import Foodbox from './components/Foodbox';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    foodsDB: foods
+  }
+
+  displayFoods = () => {
+    return this.state.foodsDB.map(food => {
+      return <Foodbox foodInfo={food} />
+    });
+  }
+  
+  render(){
+    return (
+      <div className="App">
+        {this.displayFoods()}
+      </div>
+    );
+  }
 }
 
 export default App;
