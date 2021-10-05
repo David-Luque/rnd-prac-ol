c6c60f688cb589726b8dbb4a2193e79081e00f3b
David L <dvdlq99@gmail.com>
2021-10-05 23:27:35 +0200

    Done logic in js-algorithms-prc

--- js-algorithms-prc/js/queue/queue-data-structure.js
+++ js-algorithms-prc/js/queue/queue-data-structure.js
@@ -5,23 +5,35 @@ class Queue {
   }
 
   canEnqueue() {
-    //
+    if(this.queueControl.length < this.MAX_SIZE) return true;
+    return false 
   }
 
   isEmpty() {
-    //
+    if(this.queueControl.length === 0) return true;
+    return false
   }
 
   enqueue(item) {
-    //
+    if(this.canEnqueue()) {
+      this.queueControl.unshift(item)
+      return this.queueControl
+    } else {
+      throw new Error('QUEUE_OVERFLOW')
+    }
   }
 
   dequeue() {
-    //
+    if(!this.isEmpty()) {
+      this.queueControl.shift()
+      return this.queueControl[0]
+    } else {
+      throw new Error('QUEUE_UNDERFLOW')
+    }
   }
 
   display() {
-    //
+    return this.queueControl;
   }  
 }
 
--- js-algorithms-prc/js/stack/stack-data-structure.js
+++ js-algorithms-prc/js/stack/stack-data-structure.js
@@ -5,23 +5,36 @@ class Stack {
   }
 
   canPush() {
-    // 
+    if(this.stackControl.length === this.MAX_SIZE) return false;
+    return true;
   }
 
   isEmpty() {
-    // 
+    if(this.stackControl.length === 0) return true;
+    return false;
   }
 
   push(item) {
-    // 
+    if(this.canPush()) {
+      this.stackControl.push(item)
+      return this.stackControl
+    } else {
+      throw new Error('STACK_OVERFLOW')
+    }
   }
 
   pop() {
-    // 
+    if(!this.isEmpty()) {
+      const item = this.stackControl[this.stackControl.length - 1]
+      this.stackControl.pop()
+      return item
+    } else {
+      throw new Error('STACK_UNDERFLOW')
+    }
   }
 
   display() {
-    // 
+    return this.stackControl;
   }  
 }
 
